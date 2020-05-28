b52f024eb67460bf28304b499281ebd65c24ea6b
Matheus Soares <matheus.msa27@gmail.com>
2020-05-27 23:36:36 -0300

    add port and host on wsgi configuration

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import request
 from flask_api import FlaskAPI
 
@@ -13,4 +15,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
