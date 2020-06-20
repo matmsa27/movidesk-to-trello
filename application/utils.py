0e2511d3e194f4e044b3322bf7e21a8caf144a05
Matheus Soares <matheus.msa27@gmail.com>
2020-06-20 16:39:13 -0300

    add email and name on card who created

--- application/utils.py
+++ application/utils.py
@@ -11,7 +11,21 @@ def make_card_name(data):
 
 def make_card_description(data):
     description_card = data.get("Actions")[0].get("Description")
-    return description_card
+    name = get_name_from_movidesk_json(data)
+    email = get_email_from_movidesk_json(data)
+
+    description = "{} \n {} \n {} \n".format(description_card, name, email)
+
+    return description
+
+
+def get_name_from_movidesk_json(data):
+    name = data.get("Actions")[0].get("Attachments")[0].get("CreatedBy").get("BusinessName")
+    return name
+
+def get_email_from_movidesk_json(data):
+    email = data.get("Actions")[0].get("Attachments")[0].get("CreatedBy").get("Email")
+    return email
 
 
 def check_if_card_exists(card_name_check, cards_on_board_list):
