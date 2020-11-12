d953a63807d5235235c8f3360487f329332e1e9f
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2020-11-12 16:37:25 -0300

    feat: added name, email and due_date on card creation/update

--- application/app.py
+++ application/app.py
@@ -1,19 +1,25 @@
 from flask_api import status
 
+from application.config import (
+    MOVIDESK_TOKEN,
+)
+
 from application.trello import (
     add_label_to_a_card,
     add_comment_on_card,
     create_card,
     get_cards_on_board,
+    add_due_date_on_card
 )
 from application.utils import (
     check_if_card_exists,
     make_card_description,
     make_card_name,
+    get_movidesk_ticket_info
 )
 
 
-def check_card_logical_view(data):
+def check_card_logical_view(data, due_date):
     '''
     Method to logical on check if card already exists on board
     '''
@@ -28,7 +34,8 @@ def check_card_logical_view(data):
     if card_checked:
         # If card exists, the request will insert a comment on card
         description = make_card_description(data)
-        comment_response = add_comment_on_card(card_checked, description)
+        comment_response = add_comment_on_card(card_checked["id"], description)
+        add_due_date_on_card(card_checked["id"], due_date)            
 
         if comment_response.status_code == 200:
             return comment_response.json(), status.HTTP_200_OK
@@ -66,13 +73,14 @@ def receive_webhook_from_movidesk(request):
         If checked, the request returns the response for insert a comment
         If not exists, will create a new card, and after that add the label
         '''
-        checked = check_card_logical_view(data)
+        due_date = get_movidesk_ticket_info(data.get("Id")).json().get("slaSolutionDate") + '.000Z'
+        checked = check_card_logical_view(data, due_date)
 
         if checked:
             return checked
         
         else:
-            response_trello = create_card(data)
+            response_trello = create_card(data, due_date)
 
             if response_trello.status_code == 200:
                 return add_label_logical_view(data, response_trello)
--- application/trello.py
+++ application/trello.py
@@ -27,26 +27,28 @@ def params_auth_trello():
     return params
 
 
-def payload_to_trello(data):
+def payload_to_trello(data, due_date):
 
     payload = {
         "name": make_card_name(data),
         "description": make_card_description(data),
         "label": data.get("Status"),
-        "ticket_id": data.get("Id")
+        "ticket_id": data.get("Id"),
+        "due": due_date
     }
 
     return payload
 
 
-def create_card(data):
-    payload = payload_to_trello(data)
+def create_card(data, due_date):
+    payload = payload_to_trello(data, due_date)
     url = "https://%s/%s" % (TRELLO_BASE_URL, TRELLO_CARDS)
 
     params = params_auth_trello()
     params["idList"] = TRELLO_ID_LIST
     params["name"] = payload.get("name"),
     params["desc"] = payload.get("description")
+    params["due"] = payload.get("due")
 
     try:
         response = requests.post(url=url, params=params, timeout=20)
@@ -81,6 +83,24 @@ def add_comment_on_card(card_id, comment_text):
         response = requests.post(url=url, params=params, timeout=30)
     return response
 
+
+def add_due_date_on_card(card_id, due_date):
+    url = "https://%s/%s/%s" % (
+        TRELLO_BASE_URL, TRELLO_CARDS, card_id)
+    
+    params = params_auth_trello()
+    params["due"] = due_date
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    try:
+        response = requests.put(url=url, params=params, headers=headers, timeout=20)
+    except Timeout:
+        response = requests.put(url=url, params=params, headers=headers, timeout=30)
+
+    return response
 
 def add_label_to_a_card(card_id, data):
     url = "https://%s/%s/%s/idLabels" % (
